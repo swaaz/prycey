e54b133e0165f0bdc45551d6bc979ab1323d8680
suhanacharya <suhan.acharya1@gmail.com>
2020-10-28 19:37:31 +0530

    Added partial signup and sell backend

---
+++ api/main.py
@@ -0,0 +1,65 @@
+from flask import Flask, request
+import pymongo
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+db = myclient["pryceydb"]
+item_stock = db["items"]
+users = db["users"]
+
+app = Flask(__name__)
+
+@app.route('/signin')
+def signin():
+    pass
+
+
+@app.route('/signup')
+def signup():
+    """
+        Registers new users into the users collection in db
+    """
+
+    new_user = {
+        # "username": request.form.get("username"),
+        "name": request.form.get("name"),
+        "full_name": request.form.get("fullname"),
+        "email": request.form.get("email"),
+        "contact": request.form.get("contact"),
+
+        # Pending Encrypt the password before storing in db
+        "password": request.form.get("password")
+    }
+
+    if request.form.get("email") in users.find({},{"email": 1}):
+        # return message like email already exists
+        return "error"
+    else:
+        users.insert_one(new_user)
+
+
+@app.route('/sell', methods=['POST'])
+def sell():
+    """
+        Store form details
+    """
+    new_item = {
+        "seller_id": request.args.get("seller_id"),
+        "title" : request.form.get("title"),
+        "description" : request.form.get("description"),
+        "category" : request.form.get("category"),
+        "price" : request.form.get("price"),
+        "year" : request.form.get("year")
+        # handle input images
+    }
+
+    # pending error check inputs
+    item_stock.insert_one(new_item)
+
+
+@app.route('/search')
+def search():
+    """
+        Gets search query '?q=' for name
+        Gets search other query options like sort, price range, category
+    """
+    pass
