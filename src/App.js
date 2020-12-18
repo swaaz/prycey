a0461de3acfea5a173fd0189476914476aa30cf5
swaaz <swaasthik.shetty07@gmail.com>
2020-12-18 13:00:57 +0530

    working on prouduct page

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Signup from "./components/signup/Signup";
 import Signin from "./components/signin/Signin";
 import Search from "./components/search/Search";
+import ProductPage from "./components/productPage/ProductPage";
 function App() {
   return (
     <Router>
       <div className="App">
         <Switch>
+          <Route path='/product' component={ProductPage} />
           <Route path='/search' component={Search} />
           <Route path='/signin' component={Signin} />
           <Route path='/signup' component={Signup} />
