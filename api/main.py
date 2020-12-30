9c08bf433d295ff1fc019e187ffadbb3e08d8cfa
suhanacharya <suhan.acharya1@gmail.com>
2020-12-30 12:27:57 +0530

    Changed to custom session handler

--- api/main.py
+++ api/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, session, redirect, url_for, jsonify
+from flask import Flask, request, render_template, redirect, url_for, jsonify
 from werkzeug.utils import secure_filename
 import sqlite3
 from flask_cors import CORS
@@ -12,50 +12,57 @@ db = 'prycey.db'
 # init_db()
 app = Flask(__name__)
 app.secret_key = "test"
-CORS(app)
+CORS(app, supports_credentials=True, resources={r"/*": {"origins": "*"}})
 app.config['CORS_HEADERS'] = 'Content-Type'
-app.config['CORS_RESOURCES'] = {r"*": {"origins": "*"}}
+
+sess = dict()
+
 
 # cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 
 @app.route('/signin', methods=['POST'])
 def signin():
-    print(session.get('user_id'))
+    global sess
+    if request.method == 'POST':
+        print(sess.get('user_id'))
 
-    # print(request.data)
-    request_data = request.data
-    # print(request_data)
-    cred = json.loads(request_data.decode('utf8').replace("'", '"'))[0]
-    print(cred)
-    # print(request_data['name'], request_data['password'])
+        # print(request.data)
+        request_data = request.data
+        # print(request_data)
+        cred = json.loads(request_data.decode('utf8').replace("'", '"'))
+        print(cred)
+        # print(request_data['name'], request_data['password'])
 
-    if "user_id" in session:
-        return json.dumps({"response": "Already Signed in :)"})
-    else:
-        with sqlite3.connect(db) as conn:
-            c = conn.cursor()
-            c.execute("PRAGMA FOREIGN_KEYS=ON;")
+        if "user_id" in sess:
+            print("Already Signed in :)")
+            return json.dumps({"response": "Already Signed in :)"})
+        else:
+            with sqlite3.connect(db) as conn:
+                c = conn.cursor()
+                c.execute("PRAGMA FOREIGN_KEYS=ON;")
 
-            cred_query = c.execute("""
-                                    SELECT user_id 
-                                    FROM Users
-                                    WHERE email = (?) AND password = (?);
-                                    """, tuple(cred.values())).fetchone()
+                cred_query = c.execute("""
+                                        SELECT user_id 
+                                        FROM Users
+                                        WHERE email = (?) AND password = (?);
+                                        """, tuple(cred.values())).fetchone()
 
-            if cred_query is not None:
-                session["user_id"] = cred_query[0]
-                print(session["user_id"])
-                return json.dumps({"response": "Signin Success"})
-            else:
-                return json.dumps({"response": "No Credential found"})
+                if cred_query is not None:
+                    sess["user_id"] = cred_query[0]
+                    print(sess["user_id"])
+                    print("Signin Success")
+                    return json.dumps({"response": "Signin Success"})
+                else:
+                    print("No Credential found")
+                    return json.dumps({"response": "No Credential found"})
 
 
 @app.route('/signout')
 def signout():
-    if "user_id" in session:
-        session.pop("user_id", None)
-        print("Session closed, Client Signed out")
+    if "user_id" in sess:
+        sess.pop("user_id", None)
+        print("sess closed, Client Signed out")
 
     return json.dumps({"response": "SIGN_OUT_SUCCESS"})
 
@@ -111,9 +118,9 @@ def sell():
     new_item = json.loads(request_data.decode('utf-8'))[0]
     # print(new_item)
     
-    if "user_id" in session:
+    if "user_id" in sess:
 
-        new_prod = (session["user_id"], ) + \
+        new_prod = (sess["user_id"], ) + \
             tuple(new_item.values()) + (str(datetime.date.today()), )
         # print(new_prod)
         with sqlite3.connect(db) as conn:
@@ -171,88 +178,95 @@ def search():
 #         return jsonify(users)
 
 
-# @app.route('/dashboard')
-# def render_dashboard():
-#     print(session.get('user_id'))
-#     # if "user_id" in session:
-#         with sqlite3.connect(db) as conn:
-#             c = conn.cursor()
-#             c.execute("PRAGMA FOREIGN_KEYS=ON;")
-#             details = c.execute("""
-#                                     SELECT user_id, name, email, contact_number
-#                                     FROM Users 
-#                                     WHERE user_id = (?);
-#                                 """, (session['user_id'],)).fetchone()
-#             user_posts = c.execute("""
-#                                     SELECT *
-#                                     FROM Items 
-#                                     WHERE seller_id = (?)
-#                                 """, (session['user_id'],)).fetchall()
-#             user_rating = c.execute("""
-#                                         SELECT name, rated_id, rating 
-#                                         FROM Rated, Users 
-#                                         WHERE Rated.rated_id=Users.user_id AND Rated.user_id=?
-#                                     """, (session['user_id'],)).fetchall()
-#             # print(details)
-#             # print(user_posts)
-#             user_posts = to_dict(user_posts)
-#             user_rating = rate_to_dict(user_rating)
-#             req = {
-#                 "user_id": details[0],
-#                 "name": details[1],
-#                 "email": details[2],
-#                 "contact": details[3],
-#                 "posts": user_posts,
-#                 "ratings": user_rating
-#             }
-
-#             # req = details + to_dict(user_posts)
-#             return json.dumps(req)
-#     else:
-#         return json.dumps({"response": "NOT_SIGNED_IN"})
-
 @app.route('/dashboard')
 def render_dashboard():
-    j = 'johndoe'
-    print(session.get('user_id'))
-    # if "user_id" in session:
-    with sqlite3.connect(db) as conn:
-        c = conn.cursor()
-        c.execute("PRAGMA FOREIGN_KEYS=ON;")
-        details = c.execute("""
+    global sess
+    print(sess.get('user_id'))
+    if "user_id" in sess:
+        print(sess.get('user_id'))
+
+        with sqlite3.connect(db) as conn:
+            c = conn.cursor()
+            c.execute("PRAGMA FOREIGN_KEYS=ON;")
+            details = c.execute("""
                                 SELECT Users.user_id, name, rating, no_of_ratings
                                 FROM Users, User_Rating
                                 WHERE Users.user_id=User_rating.user_id AND Users.user_id = (?);
-                            """, (j,)).fetchone()
-        user_posts = c.execute("""
-                                SELECT *
-                                FROM Items 
-                                WHERE seller_id = (?)
-                            """, (j,)).fetchall()
-        user_rating = c.execute("""
-                                    SELECT name, rated_id, rating 
-                                    FROM Rated, Users 
-                                    WHERE Rated.rated_id=Users.user_id AND Rated.user_id=?
-                                """, (j,)).fetchall()
-        # print(details)
-        # print(user_posts)
-        user_posts = to_dict(user_posts)
-        user_rating = rate_to_dict(user_rating)
-        fname, lname = details[1].split()
-        req = {
-            "user_id": details[0],
-            "fname": fname,
-            "lname": lname,
-            "rating": details[2],
-            "no_of_ratings": details[3],
-            "posts": user_posts,
-            "ratings": user_rating
-        }
+                            """, (sess.get('user_id'),)).fetchone()
+            user_posts = c.execute("""
+                                    SELECT *
+                                    FROM Items 
+                                    WHERE seller_id = (?)
+                                """, (sess.get('user_id'),)).fetchall()
+            user_rating = c.execute("""
+                                        SELECT name, rated_id, rating 
+                                        FROM Rated, Users 
+                                        WHERE Rated.rated_id=Users.user_id AND Rated.user_id=?
+                                    """, (sess.get('user_id'),)).fetchall()
+            # print(details)
+            # print(user_posts)
+            user_posts = to_dict(user_posts)
+            user_rating = rate_to_dict(user_rating)
+            fname, lname = details[1].split()
+            req = {
+                "user_id": details[0],
+                "fname": fname,
+                "lname": lname,
+                "rating": details[2],
+                "no_of_ratings": details[3],
+                "posts": user_posts,
+                "ratings": user_rating
+            }
 
-        # req = details + to_dict(user_posts)
-        return json.dumps(req)
-    # else:
-    #     return json.dumps({"response": "NOT_SIGNED_IN"})
+            # req = details + to_dict(user_posts)
+            return json.dumps(req)
+    else:
+        return json.dumps({"response": "NOT_SIGNED_IN"})
+
+# @app.route('/dashboard')
+# def render_dashboard():
+#     j = 'jennaf'
+#     global sess
+#     print(sess)
+#     print(sess.get('user_id'))
+#     # if "user_id" in sess:
+#     with sqlite3.connect(db) as conn:
+#         c = conn.cursor()
+#         c.execute("PRAGMA FOREIGN_KEYS=ON;")
+#         details = c.execute("""
+#                                 SELECT Users.user_id, name, rating, no_of_ratings
+#                                 FROM Users, User_Rating
+#                                 WHERE Users.user_id=User_rating.user_id AND Users.user_id = (?);
+#                             """, (j,)).fetchone()
+#         user_posts = c.execute("""
+#                                 SELECT *
+#                                 FROM Items 
+#                                 WHERE seller_id = (?)
+#                             """, (j,)).fetchall()
+#         user_rating = c.execute("""
+#                                     SELECT name, rated_id, rating 
+#                                     FROM Rated, Users 
+#                                     WHERE Rated.rated_id=Users.user_id AND Rated.user_id=?
+#                                 """, (j,)).fetchall()
+#         # print(details)
+#         # print(user_posts)
+#         user_posts = to_dict(user_posts)
+#         user_rating = rate_to_dict(user_rating)
+#         fname, lname = details[1].split()
+#         req = {
+#             "user_id": details[0],
+#             "fname": fname,
+#             "lname": lname,
+#             "rating": details[2],
+#             "no_of_ratings": details[3],
+#             "posts": user_posts,
+#             "ratings": user_rating
+#         }
+
+#         # req = details + to_dict(user_posts)
+#         return json.dumps(req)
+#     # else:
+#     #     return json.dumps({"response": "NOT_SIGNED_IN"})
 
 
 @app.route('/product/<int:id>')
@@ -288,7 +302,7 @@ def edit_product(id):
         Edits 
     """
 
-    if "user_id" in session:
+    if "user_id" in sess:
         request_data = request.data
         new_item = json.loads(request_data.decode('utf-8'))[0]
         # print(new_item)
@@ -300,7 +314,7 @@ def edit_product(id):
             k = c.execute(
                 """SELECT seller_id FROM Items WHERE item_id = ?""", (id, )).fetchone()
             # print(k)
-            if k[0] == session["user_id"]:
+            if k[0] == sess["user_id"]:
                 c.execute("""UPDATE Items
                             SET 
                             title = ?,
@@ -326,7 +340,7 @@ def edit_product(id):
 
 @app.route('/product/<int:id>/delete')
 def delete_product(id):
-    # if "user_id" in session:
+    # if "user_id" in sess:
     with sqlite3.connect(db) as conn:
         c = conn.cursor()
         c.execute("PRAGMA FOREIGN_KEYS=ON;")
@@ -334,7 +348,7 @@ def delete_product(id):
         k = c.execute(
             """SELECT seller_id FROM Items WHERE item_id = ?""", (id, )).fetchone()
 
-        # if k[0] == session["user_id"]:
+        # if k[0] == sess["user_id"]:
         if k[0] == 'johndoe':
             c.execute("""DELETE FROM Items WHERE item_id = ?""", (id, ))
             conn.commit()
@@ -367,7 +381,7 @@ def prod_cat(cid):
 @app.route('/rating/<string:uid>')
 def rate(uid):
     nr = float(request.args.get('r'))
-    if "user_id" in session:
+    if "user_id" in sess:
         if 1 <= nr <= 5:
             with sqlite3.connect(db) as conn:
                 c = conn.cursor()
@@ -376,11 +390,11 @@ def rate(uid):
                 try:
                     c.execute("""INSERT INTO Rated(user_id, rated_id, rating) 
                                 VALUES(?,?,?);
-                                """, (session["user_id"], uid, nr))
+                                """, (sess["user_id"], uid, nr))
                     conn.commit()
                 except sqlite3.IntegrityError:
                     c.execute("""UPDATE Rated SET rating=? WHERE user_id=? AND rated_id=?;
-                                """, (nr, session["user_id"], uid, ))
+                                """, (nr, sess["user_id"], uid, ))
                     conn.commit()
 
                 return json.dumps({"response": "RATING_UPDATED"})
